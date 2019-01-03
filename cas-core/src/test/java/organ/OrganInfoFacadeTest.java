26dc1aa194c32418264c95c0d40d0e48ca0c595e
leixiaorui <1150992388@qq.com>
2019-01-03 16:49:42 +0800

    与MCS联调测试

--- cas-core/src/test/java/organ/OrganInfoFacadeTest.java
+++ cas-core/src/test/java/organ/OrganInfoFacadeTest.java
@@ -5,9 +5,12 @@ import com.msjf.finance.cas.common.test.SpringTestCase;
 
 import com.msjf.finance.cas.facade.employee.EmployeeInfoFacade;
 import com.msjf.finance.cas.facade.organ.OrganInfoFacade;
+import com.msjf.finance.cas.facade.organ.domain.OrganInfoDomain;
+import com.msjf.finance.mcs.facade.sms.SendVerificationCodeFacade;
 import org.junit.Test;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 
 
 /**
@@ -15,17 +18,24 @@ import javax.annotation.Resource;
  */
 
 public class OrganInfoFacadeTest   extends SpringTestCase{
-/*    @Resource
-    OrganInfoFacade organInfoFacade;*/
-@Resource
-    EmployeeInfoFacade employeeInfoFacade;
+   @Resource
+   SendVerificationCodeFacade sendVerificationCodeFacade;
+/*@Resource
+    EmployeeInfoFacade employeeInfoFacade;*/
 
     @Test
     public  void  queryOrganInfoList(){
-        System.out.println(  employeeInfoFacade.queryEmployeeInfoList());
-     //   System.out.println("--------------------------"+organInfoFacade.queryOrganInfoList());
-        while (true){
+
+     //   System.out.println(  employeeInfoFacade.queryEmployeeInfoList())
+      try {
+            Thread.sleep(3000);
+          //  System.out.println("-------------queryOrganInfoList-------------" + organInfoFacade.queryOrganInfoList(new OrganInfoDomain()));
+          System.out.println( "****************************:"+sendVerificationCodeFacade.SendRegisterVerificationCode(new HashMap<>()));
+        }catch (Exception e){
 
         }
+/*        while (true){
+
+        }*/
     }
 }
