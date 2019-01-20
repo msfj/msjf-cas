524c5b88b739edfd1f3c3f48e016cadde4e51440
csw2 <857495414@qq.com>
2019-01-20 14:47:15 +0800

    编译报错修改

---
+++ cas-core/src/main/java/com/msjf/finance/cas/modules/Account/AccountDao.java
@@ -0,0 +1,16 @@
+package com.msjf.finance.cas.modules.Account;
+
+
+import com.msjf.finance.msjf.core.dao.MyBatisDao;
+
+import java.util.HashMap;
+import java.util.List;
+@MyBatisDao
+public interface AccountDao {
+
+    List selectOrganInfoByMobile(HashMap map);
+
+    List selectLoginInfo(HashMap map);
+
+    List selectEmployeeInfo(HashMap map);
+}
