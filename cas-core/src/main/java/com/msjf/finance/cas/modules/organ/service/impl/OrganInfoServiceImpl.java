d584a850c877bf3ad2685a5f95c7c85116fb74f0
lrkkevin <lrkkevin@163.com>
2019-01-20 21:39:12 +0800

    增加企业基本信息抽象类

--- cas-core/src/main/java/com/msjf/finance/cas/modules/organ/service/impl/OrganInfoServiceImpl.java
+++ cas-core/src/main/java/com/msjf/finance/cas/modules/organ/service/impl/OrganInfoServiceImpl.java
@@ -7,6 +7,7 @@ import com.msjf.finance.cas.modules.organ.dao.OrganInfoDao;
 import com.msjf.finance.cas.modules.organ.emun.OrganInfoEmun;
 import com.msjf.finance.cas.modules.organ.entity.OrganInfoEntity;
 import com.msjf.finance.cas.modules.organ.entity.OrganInfoKey;
+import com.msjf.finance.cas.modules.organ.service.AbstractOrganInfo;
 import com.msjf.finance.cas.modules.organ.service.OrganInfoService;
 import com.msjf.finance.cas.modules.util.CommonUtil;
 import com.msjf.finance.cas.modules.util.StringUtil;
@@ -28,27 +29,27 @@ import java.util.Map;
  */
 @Service("organInfoService")
 @Scope("prototype")
-public class OrganInfoServiceImpl extends Account implements OrganInfoService {
-   @Resource
-   OrganInfoDao organInfoDao;
-   @Resource
+public class OrganInfoServiceImpl extends AbstractOrganInfo {
+    @Resource
+    OrganInfoDao organInfoDao;
+    @Resource
     AccountDao accountDao;
-           private String customerno;
-        public List queryOrganInfoList() {
-            try {
-                List<OrganInfoEntity> organInfoEntityList = organInfoDao.queryOrganInfoList();
-                List<OrganInfoDomain> organInfoDomainList = new ArrayList();
-                organInfoEntityList.stream().forEach(organInfoEntity ->
-                        {
-                            OrganInfoDomain organInfoDomain = new OrganInfoDomain();
-                            BeanUtils.copyProperties(organInfoEntity, organInfoDomain);
-                            organInfoDomainList.add(organInfoDomain);
-                        }
-                );
-                return organInfoDomainList;
-            }catch (Exception e){
-                //打印错误日志
-           e.printStackTrace();
+    private String customerno;
+
+    @Override
+    public List queryOrganInfoList() {
+        try {
+            List<OrganInfoEntity> organInfoEntityList = organInfoDao.queryOrganInfoList();
+            List<OrganInfoDomain> organInfoDomainList = new ArrayList();
+            organInfoEntityList.stream().forEach(organInfoEntity -> {
+                OrganInfoDomain organInfoDomain = new OrganInfoDomain();
+                BeanUtils.copyProperties(organInfoEntity, organInfoDomain);
+                organInfoDomainList.add(organInfoDomain);
+            });
+            return organInfoDomainList;
+        } catch (Exception e) {
+            //打印错误日志
+            e.printStackTrace();
         }
         return null;
     }
@@ -73,10 +74,12 @@ public class OrganInfoServiceImpl extends Account implements OrganInfoService {
 //        try {
 //            Map<String, String> infoMap = accountDao.selectCompanyPersonInfo(paramMap);
 //            if(!StringUtils.isEmpty(infoMap.get("caremarkettype"))){
-//                infoMap.put("caremarkettypename", CommonUtil.getDictValueBatch("dict20025",infoMap.get("caremarkettype")));
+//                infoMap.put("caremarkettypename", CommonUtil.getDictValueBatch("dict20025",infoMap.get
+// ("caremarkettype")));
 //            }
 ////			if(!CheckUtil.isNull(infoMap.get("fundprojecttrade"))){
-////			    infoMap.put("fundprojecttradename",CommonUtil.getDictValueBatch("dict20025",infoMap.get("fundprojecttrade")));
+////			    infoMap.put("fundprojecttradename",CommonUtil.getDictValueBatch("dict20025",infoMap.get
+// ("fundprojecttrade")));
 ////			}
 //            if(!StringUtils.isEmpty(infoMap.get("companytrade"))){
 //                infoMap.put("companytradename",CommonUtil.getDictValueBatch("dict20025",infoMap.get("companytrade")));
@@ -85,10 +88,12 @@ public class OrganInfoServiceImpl extends Account implements OrganInfoService {
 //                infoMap.put("projecttradename",CommonUtil.getDictValueBatch("dict20025",infoMap.get("projecttrade")));
 //            }
 ////			if(!CheckUtil.isNull(infoMap.get("fundprojectphase"))){
-////				infoMap.put("fundprojectphasename",CommonUtil.getDictValueBatch("dict20026",infoMap.get("fundprojectphase")));
+////				infoMap.put("fundprojectphasename",CommonUtil.getDictValueBatch("dict20026",infoMap.get
+// ("fundprojectphase")));
 ////			}
 //            if(!StringUtils.isEmpty(infoMap.get("projectphase"))) {
-//                infoMap.put("projectphasename", CommonUtil.getDictValueBatch("dict20026", infoMap.get("projectphase")));
+//                infoMap.put("projectphasename", CommonUtil.getDictValueBatch("dict20026", infoMap.get
+// ("projectphase")));
 //            }
 //            ResultUtil.makerSusResults("查询成功", infoMap, rs);
 //        } catch (Exception e) {
