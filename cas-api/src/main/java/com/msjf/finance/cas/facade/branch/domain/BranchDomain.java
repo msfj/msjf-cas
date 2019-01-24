499734783d979c1214ac202d660bd6076a02d774
chengjunping <153449986@qq.com>
2019-01-24 21:18:17 +0800

    部门相关接口

---
+++ cas-api/src/main/java/com/msjf/finance/cas/facade/branch/domain/BranchDomain.java
@@ -0,0 +1,127 @@
+package com.msjf.finance.cas.facade.branch.domain;
+
+import com.msjf.finance.msjf.core.domian.BaseDomain;
+
+/**
+ * Created by Jsan on 2019/1/24.
+ */
+public class BranchDomain extends BaseDomain {
+    private String branchid;
+
+    private String branchname;
+
+    private String parentid;
+
+    private String branchtype;
+
+    private String province;
+
+    private String city;
+
+    private String county;
+
+    private String stertreet;
+
+    private int showorder;
+
+    private int curnumber;
+
+    private Integer pageSize;
+    private Integer pageNumber;
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Integer getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(Integer pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public String getBranchid() {
+        return branchid;
+    }
+
+    public void setBranchid(String branchid) {
+        this.branchid = branchid;
+    }
+
+    public String getBranchname() {
+        return branchname;
+    }
+
+    public void setBranchname(String branchname) {
+        this.branchname = branchname;
+    }
+
+    public String getParentid() {
+        return parentid;
+    }
+
+    public void setParentid(String parentid) {
+        this.parentid = parentid;
+    }
+
+    public String getBranchtype() {
+        return branchtype;
+    }
+
+    public void setBranchtype(String branchtype) {
+        this.branchtype = branchtype;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getStertreet() {
+        return stertreet;
+    }
+
+    public void setStertreet(String stertreet) {
+        this.stertreet = stertreet;
+    }
+
+    public int getShoworder() {
+        return showorder;
+    }
+
+    public void setShoworder(int showorder) {
+        this.showorder = showorder;
+    }
+
+    public int getCurnumber() {
+        return curnumber;
+    }
+
+    public void setCurnumber(int curnumber) {
+        this.curnumber = curnumber;
+    }
+}
---
+++ cas-core/src/main/java/com/msjf/finance/cas/modules/branch/service/impl/BranchServiceImpl.java
@@ -0,0 +1,131 @@
+package com.msjf.finance.cas.modules.branch.service.impl;
+
+
+import com.msjf.finance.cas.common.dao.entity.BranchEntity;
+import com.msjf.finance.cas.common.dao.entity.EmployeeEntity;
+import com.msjf.finance.cas.common.dao.key.BranchKey;
+import com.msjf.finance.cas.common.dao.persistence.BranchDao;
+import com.msjf.finance.cas.common.dao.persistence.EmployeeDao;
+import com.msjf.finance.cas.common.utils.StringUtil;
+import com.msjf.finance.cas.modules.branch.service.BranchService;
+import com.msjf.finance.msjf.core.response.Response;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.msjf.finance.cas.modules.Account.Account.BRANCH_TYPE_BM;
+import static com.msjf.finance.cas.modules.Account.Account.BRANCH_TYPE_JG;
+
+/**
+ * Created by Jsan on 2019/1/24.
+ */
+
+@Service("branchService")
+public class BranchServiceImpl implements BranchService {
+
+    @Resource
+    BranchDao branchDao;
+
+    @Resource
+    EmployeeDao employeeInfoDao;
+
+    @Override
+    public Response insBranchInfo(BranchEntity branchEntity) throws RuntimeException{
+        Response re = new Response();
+        try {
+            int i = branchDao.insEntity(branchEntity);
+            re.success("1","操作成功",i);
+        }catch (Exception e){
+            e.printStackTrace();
+            re.fail("0","操作失败");
+        }
+
+        return re;
+    }
+
+    @Override
+    public Response delBranchInfo(Map<String, Object> mapParams) throws RuntimeException{
+        Response re = new Response();
+        if(StringUtils.isEmpty(mapParams.get("branchid"))){
+            return new Response().fail("0","branchid不能为空");
+        }
+        String branchid = (String)mapParams.get("branchid");
+        try {
+            BranchEntity b = new BranchEntity();
+            b.setBranchid(branchid);
+            List<BranchEntity> list = branchDao.getListEntity(b);
+            if(ObjectUtils.isEmpty(list)){
+                return new Response().fail("0","操作失败,当前部门id不存在");
+            }
+            BranchEntity branchEntity = list.get(0);
+            if(BRANCH_TYPE_JG.equals(branchEntity.getBranchtype())){
+                return new Response().fail("0","操作失败,不能删除机构");
+            }
+            Map<String, Object> parentMap = new HashMap();
+            BranchEntity c = new BranchEntity();
+            c.setParentid(branchEntity.getBranchid());
+            List<BranchEntity> sonlist = branchDao.getListEntity(c);
+            if(!ObjectUtils.isEmpty(sonlist)){
+                return new Response().fail("0","操作失败,当前部门存在子部门，请先删除子部门后再进行删除");
+            }
+            EmployeeEntity entity = new EmployeeEntity();
+            entity.setBranchid(branchEntity.getBranchid());
+            List<EmployeeEntity> emlist = employeeInfoDao.getListEntity(entity);
+            if(!ObjectUtils.isEmpty(emlist)){
+                return new Response().fail("0","操作失败,当前部门存在员工，请先删除员工后再进行删除");
+            }
+            BranchKey key = new BranchKey();
+            key.setBranchid(branchEntity.getBranchid());
+            int i = branchDao.delEntityKey(key);
+            re.success("1","删除成功",i);
+        }catch (Exception e){
+            e.printStackTrace();
+            re.fail("0","操作失败");
+        }
+
+        return re;
+    }
+
+    @Override
+    public Response updBranchInfo(BranchEntity branchEntity) throws RuntimeException{
+        Response re = new Response();
+        try {
+            BranchEntity b =new BranchEntity();
+            b.setBranchid(branchEntity.getBranchid());
+            List<BranchEntity> list = branchDao.getListEntity(b);
+            Integer i = 0;
+            if(ObjectUtils.isEmpty(list)){
+                branchEntity.setBranchid(StringUtil.getUUID());
+                branchEntity.setBranchtype(BRANCH_TYPE_BM);
+                i= branchDao.insEntity(branchEntity);
+            }else{
+                i=branchDao.updEntity(branchEntity);
+            }
+            re.success("1","操作成功",i);
+        }catch (Exception e){
+            e.printStackTrace();
+            re.fail("0","操作失败");
+        }
+
+        return re;
+    }
+
+    @Override
+    public Response queryBranch(Map<String, Object> mapParams) throws RuntimeException{
+        Response re = new Response();
+        try {
+            List<BranchEntity> list = branchDao.queryBranch(mapParams);
+            re.success("1","操作成功",list);
+        }catch (Exception e){
+            e.printStackTrace();
+            re.fail("0","操作失败");
+        }
+
+        return re;
+    }
+}
