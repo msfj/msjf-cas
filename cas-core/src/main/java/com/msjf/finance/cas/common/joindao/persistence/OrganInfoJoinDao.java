49425ab7026736ccdd51c1802214b4cc7ce994a1
lrkkevin <lrkkevin@163.com>
2019-01-26 10:08:41 +0800

    增加自定义dao文件目录

---
+++ cas-core/src/main/java/com/msjf/finance/cas/common/joindao/persistence/OrganInfoJoinDao.java
@@ -0,0 +1,12 @@
+package com.msjf.finance.cas.common.joindao.persistence;
+
+/**
+ * <pre>
+ * 描述:
+ *    ${DESCRIPTION}
+ *  <pre/>
+ * @author 95494
+ * @create 2019-01-26 10:07
+ */
+public interface OrganInfoJoinDao {
+}
