cb0386be233d821192ba82b01bf3d305991756f9
chengjunping <153449986@qq.com>
2019-01-26 14:35:33 +0800

    domain修改

--- cas-api/src/main/java/com/msjf/finance/cas/facade/branch/domain/BranchDomain.java
+++ cas-api/src/main/java/com/msjf/finance/cas/facade/branch/domain/BranchDomain.java
@@ -26,25 +26,6 @@ public class BranchDomain extends BaseDomain {
 
     private int curnumber;
 
-    private Integer pageSize;
-    private Integer pageNumber;
-
-    public Integer getPageSize() {
-        return pageSize;
-    }
-
-    public void setPageSize(Integer pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    public Integer getPageNumber() {
-        return pageNumber;
-    }
-
-    public void setPageNumber(Integer pageNumber) {
-        this.pageNumber = pageNumber;
-    }
-
     public String getBranchid() {
         return branchid;
     }
