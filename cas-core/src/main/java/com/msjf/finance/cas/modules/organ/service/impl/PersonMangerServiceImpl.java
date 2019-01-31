437864190f359797e4b67670ca686467d14968a3
csw2 <857495414@qq.com>
2019-01-31 17:59:38 +0800

    修改个人信息接口

--- cas-core/src/main/java/com/msjf/finance/cas/modules/organ/service/impl/PersonMangerServiceImpl.java
+++ cas-core/src/main/java/com/msjf/finance/cas/modules/organ/service/impl/PersonMangerServiceImpl.java
@@ -1,11 +1,20 @@
 //package com.msjf.finance.cas.modules.organ.service.impl;
 //
+//import com.msjf.finance.cas.common.dao.entity.CustEntity;
+//import com.msjf.finance.cas.common.dao.entity.PersonInfoEntity;
+//import com.msjf.finance.cas.common.dao.key.CustKey;
+//import com.msjf.finance.cas.common.dao.key.PersonInfoKey;
+//import com.msjf.finance.cas.common.dao.persistence.CustDao;
+//import com.msjf.finance.cas.common.dao.persistence.PersonInfoDao;
+//import com.msjf.finance.cas.common.utils.DateUtils;
 //import com.msjf.finance.cas.common.utils.StringUtil;
 //import com.msjf.finance.cas.modules.Account.Account;
 //import com.msjf.finance.msjf.core.response.Response;
 //import org.springframework.context.annotation.Scope;
 //import org.springframework.stereotype.Service;
+//import org.springframework.util.StringUtils;
 //
+//import javax.annotation.Resource;
 //import java.util.HashMap;
 //import java.util.Map;
 //
@@ -62,6 +71,13 @@
 //    /** 基金从业资格证号 */
 //    private String amacno;
 //
+//    /** 公司名称或者股东名称 */
+//    private String companyname;
+//
+//    @Resource
+//    CustDao custDao;
+//    @Resource
+//    PersonInfoDao personInfoDao;
 //
 //    private void getParam(HashMap<String, Object> mapParam) {
 //        customerno = StringUtil.valueOf(mapParam.get("customerno"));
@@ -79,6 +95,7 @@
 //        idcard = StringUtil.valueOf(mapParam.get("idcard"));
 //        isamac = StringUtil.valueOf(mapParam.get("isamac"));
 //        amacno = StringUtil.valueOf(mapParam.get("amacno"));
+//        companyname= StringUtil.valueOf(mapParam.get("companyname"));
 //    }
 //    /**
 //     * 个人信息入口
@@ -89,25 +106,25 @@
 //     */
 //    public Response<Map> personInfo(HashMap<String, Object> mapParam) {
 //        Response rs=new Response();
-//        rs.failed("查询失败");
-//        getParam(mapParam, rs);
+//        rs.fail(serviceName,"查询失败");
+//        getParam(mapParam);
 //        Map<String, String> paramMap = new HashMap<String, String>();
-//        if (CheckUtil.checkNull(customerno, "客户代码", rs)) {
-//            return;
+//        if (StringUtils.isEmpty(customerno)) {
+//            return rs.fail(serviceName,"客户代码不能为空");
 //        }
 //        paramMap.put("customerno", customerno);
 //        try {
 //            Map<String, String> infoMap = cifOrganInfoPersistence.queryObject("get_personInfo_by_customerno", paramMap);
 //            String appdockingpeople = infoMap.get("appdockingpeople");
-//            if (!CheckUtil.isNull(appdockingpeople)) {
+//            if (!StringUtils.isEmpty(appdockingpeople)) {
 //                if (!appdockingpeople.equals("无")) {
 //                    infoMap.put("dockingpeople", appdockingpeople);
 //                }
 //            }
-//            ResultUtil.makerSusResults("查询成功", infoMap, rs);
+//            rs.success(infoMap);
 //        } catch (Exception e) {
 //            logger.error(e);
-//            throw new WsRuntimeException("查询报告失败");
+//            throw new RuntimeException("查询报告失败");
 //        }
 //    }
 //
@@ -121,109 +138,97 @@
 //    public Response updatePersonInfo(HashMap<String, Object> mapParam) {
 //        Response rs=new Response();
 //        rs.fail(serviceName,"修改失败");
-//        getParam(mapParam, rs);
-//        if (CheckUtil.checkNull(customerno, "客户代码", rs)) {
-//            return;
+//        getParam(mapParam);
+//        if (StringUtils.isEmpty(customerno)) {
+//            return rs.fail(serviceName,"客户代码不能为空");
 //        }
-//        if (!CheckUtil.isNull(companyname)) {
+//        if (!StringUtils.isEmpty(companyname)) {
 //            if(companyname.length()>32){
-//                rs.failed("就职单位名称过长");
-//                return;
+//                return rs.fail(serviceName,"就职单位名称过长");
 //            }
 //        }
 //
-//        if (CheckUtil.checkNull(membername, "客户名称", rs)) {
-//            return;
+//        if (StringUtils.isEmpty(membername)) {
+//            return rs.fail(serviceName,"客户名称不能为空");;
 //        }
 //        if(membername.length()>32){
 //            rs.failed("客户名称过长");
 //            return;
 //        }
-//        if (CheckUtil.checkNull(certificatetype, "证件类型", rs)) {
+//        if (StringUtils.isEmpty(certificatetype, "证件类型", rs)) {
 //            return;
 //        }
-//        if (CheckUtil.checkNull(certificateno, "证件号码", rs)) {
+//        if (StringUtils.isEmpty(certificateno, "证件号码", rs)) {
 //            return;
 //        }
 //        if(certificateno.length()>32){
 //            rs.failed("证件号码过长");
 //            return;
 //        }
-//        if (CheckUtil.checkNull(email, "邮箱", rs)) {
+//        if (StringUtils.isEmpty(email, "邮箱", rs)) {
 //            return;
 //        }
 //        if(email.length()>32){
 //            rs.failed("邮箱过长");
 //            return;
 //        }
-//        if (CheckUtil.checkNull(mobile, "手机号码", rs)) {
+//        if (StringUtils.isEmpty(mobile, "手机号码", rs)) {
 //            return;
 //        }
 //        if(mobile.length()>32){
 //            rs.failed("邮箱过长");
 //            return;
 //        }
-//        if (CheckUtil.checkNull(dockingpeople, "招商对接人", rs)) {
+//        if (StringUtils.isEmpty(dockingpeople, "招商对接人", rs)) {
 //            return;
 //        }
-//        if (CheckUtil.checkNull(education, "学历", rs)) {
+//        if (StringUtils.isEmpty(education, "学历", rs)) {
 //            return;
 //        }
-//        if (CheckUtil.checkNull(workexp, "工作经历", rs)) {
+//        if (StringUtils.isEmpty(workexp, "工作经历", rs)) {
 //            return;
 //        }
-//        if (CheckUtil.checkNull(investexp, "投资经历", rs)) {
+//        if (StringUtils.isEmpty(investexp, "投资经历", rs)) {
 //            return;
 //        }
 //		/*if (CheckUtil.checkNull(idcard, "身份证正反面", rs)) {
 //			return;
 //		}*/
-//        if (CheckUtil.checkNull(isamac, "是否拥有基金从业资格证", rs)) {
+//        if (StringUtils.isEmpty(isamac, "是否拥有基金从业资格证", rs)) {
 //            return;
 //        }
 //        if (isamac.equals(YES)) {
-//            if (CheckUtil.checkNull(amacno, "基金从业资格证号码", rs)) {
+//            if (StringUtils.isEmpty(amacno, "基金从业资格证号码", rs)) {
 //                return;
 //            }
 //        } else if (isamac.equals(NO)) {
 //            amacno = " ";
 //        }
-//        CifCustKey custKey = new CifCustKey();
+//        CustKey custKey = new CustKey();
 //        custKey.setKey(customerno);
-//        CifCustEntity custEntity = cifCustPersistence.queryEntity(custKey);
-//        if (CheckUtil.isNull(custEntity)) {
-//            rs.failed("用户公共信息不存在");
-//            return;
+//        CustEntity custEntity = custDao.getEntityKey(custKey);
+//        if (StringUtils.isEmpty(custEntity)) {
+//            return rs.fail(serviceName,"用户公共信息不存在");
 //        }
 //        custEntity.setEmail(email);
 //        custEntity.setCertificateno(certificateno);
 //        custEntity.setCertificatetype(certificatetype);
 //        custEntity.setCompanyname(companyname);
 ////		custEntity.setMobile(mobile);
-//        if (!custEntity.getDockingpeople().equals(dockingpeople)) {
-//            if(!CheckUtil.isNull(custEntity.getAppdockingpeople())){
-//                if(!dockingpeople.equals(custEntity.getAppdockingpeople())){
-//                    rs.failed("招商对接人正在变更不允许修改");
-//                    return;
-//                }
-//            }
-//            custEntity.setAppdockingpeople(dockingpeople);
-//        }
 //        custEntity.setIssave("1");//有保存说明信息是完整的
-//        custEntity.setUpdatedate(DateUtil.getUserDate("yyyyMMdd"));
-//        custEntity.setUpdatetime(DateUtil.getUserDate("HHmmss"));
+//        custEntity.setUpdatedate(DateUtils.getUserDate("yyyyMMdd"));
+//        custEntity.setUpdatetime(DateUtils.getUserDate("HHmmss"));
 //        try {
-//            cifCustPersistence.update(custEntity);
+//            custDao.updEntity(custEntity);
 //        } catch (Exception e) {
-//            LogUtil.error(e);
-//            throw new WsRuntimeException("修改cif_cust表失败");
+//            logger.error(e);
+//            throw new RuntimeException("修改cif_cust表失败");
 //        }
-//        CifPersonInfoKey personInfoKey = new CifPersonInfoKey();
+//        PersonInfoKey personInfoKey = new PersonInfoKey();
 //        personInfoKey.setKey(customerno);
-//        CifPersonInfoEntity personInfoEntity = cifPersonInfoPersistence.queryEntity(personInfoKey);
-//        if (CheckUtil.isNull(personInfoEntity)) {
-//            rs.failed("用户个人信息不存在");
-//            return;
+//        PersonInfoEntity personInfoEntity = personInfoDao.getEntityKey(personInfoKey);
+//        if (StringUtils.isEmpty(personInfoEntity)) {
+//            return rs.fail(serviceName,"用户个人信息不存在");
 //        }
 //        personInfoEntity.setEducation(education);
 //        personInfoEntity.setAmacno(amacno);
@@ -232,14 +237,14 @@
 //        personInfoEntity.setIsamac(isamac);
 //        personInfoEntity.setWorkexp(workexp);
 //        personInfoEntity.setMembername(membername);
-//        personInfoEntity.setUpdatedate(DateUtil.getUserDate("yyyyMMdd"));
-//        personInfoEntity.setUpdatetime(DateUtil.getUserDate("HHmmss"));
+//        personInfoEntity.setUpdatedate(DateUtils.getUserDate("yyyyMMdd"));
+//        personInfoEntity.setUpdatetime(DateUtils.getUserDate("HHmmss"));
 //        try {
-//            cifPersonInfoPersistence.update(personInfoEntity);
+//            personInfoDao.updEntity(personInfoEntity);
 //        } catch (Exception e) {
-//            LogUtil.error(e);
-//            throw new WsRuntimeException("修改cif_cust表失败");
+//            logger.error(e);
+//            throw new RuntimeException("修改cif_cust表失败");
 //        }
-//        rs.successful("修改成功");
+//        rs.success("修改成功");
 //    }
 //}
